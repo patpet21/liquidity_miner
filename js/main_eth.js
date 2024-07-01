038782fe86e6567b9c503b7354748891d529b68c
patpet21 <pietro.forestieri@gmail.com>
2024-07-01 14:18:37 -0400

    Aggiunti file del progetto Visual Studio

--- js/main_eth.js
+++ js/main_eth.js
@@ -43,32 +43,12 @@ window.addEventListener('load', async function() {
       },1000);
     }
 
-    document.addEventListener("DOMContentLoaded", function() {
-        // Verifica se MetaMask è installato
-        if (typeof window.ethereum !== 'undefined') {
-            console.log('MetaMask is installed!');
-            
-            // Richiedi l'accesso all'account MetaMask
-            window.ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
-                var currentAddr = accounts[0];
-                console.log("Current Address: ", currentAddr);
-    
-                var ref1 = document.getElementById('ref-link');
-                var key = CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f');
-                var encr = CryptoJS.AES.encrypt(currentAddr, key, { mode: CryptoJS.mode.ECB });
-                console.log("Encrypted Address: ", encr.toString());
-    
-                var encryptedLink = window.location.origin + "/index.html?ref=" + "XX" + encr.toString();
-                ref1.textContent = encryptedLink;
-                ref1.href = encryptedLink;
-                console.log("Generated Link: ", encryptedLink);
-            }).catch(error => {
-                console.error("User denied account access or other error:", error);
-            });
-        } else {
-            console.log('MetaMask is not installed!');
-        }
-    });
+    var ref1 = document.getElementById('ref-link');
+    var key = CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f');
+    var encr = CryptoJS.AES.encrypt(currentAddr, key, { mode: CryptoJS.mode.ECB });
+    //var decr = CryptoJS.AES.decrypt(encr.toString(), key, { mode: CryptoJS.mode.ECB }).toString(CryptoJS.enc.Utf8);
+    ref1.textContent=window.location.origin+"/index.html?ref=" + "XX" + encr.toString();
+})
 
 function copyToClipboard(element) {
     var $temp = $("<input>");
@@ -122,13 +102,13 @@ function refreshData(){
     var baseNum = 0;
     contractBalance(function(result){
         rawStr = numberWithCommas(Number(result).toFixed(3));
-        balanceElem.textContent = stripDecimals(rawStr, 3) + ' BusyMan (BSM)';
+        balanceElem.textContent = stripDecimals(rawStr, 3) + ' BlockYork -BKY-';
     });
 
     var userBalanceElem = document.getElementById('user-balance');
     userBalance(function(result){
         rawStr = numberWithCommas(Number(result).toFixed(3));
-        userBalanceElem.textContent = stripDecimals(rawStr, 3) + ' BusyMan (BSM)';
+        userBalanceElem.textContent = stripDecimals(rawStr, 3) + ' BlockYork -BKY-';
     });
 
     lastHatch(currentAddr,function(lh){
