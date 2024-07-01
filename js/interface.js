5380ae4bc28df47b5f09a7e56113eb86fb2ef25b
patpet21 <pietro.forestieri@gmail.com>
2024-07-01 15:06:53 -0400

    Aggiunti file del progetto Visual Studio

--- js/interface.js
+++ js/interface.js
@@ -642,10 +642,10 @@ function userBalance(callback){
 
 function buyEggs(ref, trx, callback){
 	if(+trx > +usrBal) {
-		alert("You don't have " + trx + " BusyMan (BSM) in your wallet");
+		alert("You don't have " + trx + " MEXA -$MXA- in your wallet");
 	}
 	else if(+trx > +spend) {
-		alert("Approve spending " + trx + " BusyMan (BSM) first");
+		alert("Approve spending " + trx + " MEXA -$MXA- first");
 	} else {
 			minersContract.methods.buyEggs(ref, web3.utils.toWei(trx)).send({ from:currentAddr }).then(result => {
         callback();
